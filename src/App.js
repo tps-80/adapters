d25a7defd6cf38ab1c317c16e396d8a0192e1f1e
Thomas Sorensen <12405751+tps-80@users.noreply.github.com>
2018-10-09 23:23:56 -0700

    adding state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const allDevices = [
+  {
+    id: 0,
+    type: "Laptop",
+  },
+  {
+    id: 1,
+    type: "Desktop",
+  },
+  {
+    id: 2,
+    type: "iPad"
+  },
+]
+
+const userDevice = {
+    id: 1,
+    type: "unknown",
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allDevices,
+      userDevice,
+    };
+  }
+
+  logger() { console.log("you clicked something") }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Which device do you have?
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div onClick={this.logger}>
+            Laptop
+          </div>
+          <div>
+            Desktop
+          </div>
         </header>
+        <h3>You might have a <br/>
+          {this.state.userDevice.type}
+        </h3>
       </div>
     );
   }
